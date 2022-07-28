02d1785d31a71949695d3057266705b5948d8e3a
Konstantin Krukhmalev <shader42@gmail.com>
2022-07-28 18:51:17 +0300

    Add annotation levels

--- src/main.py
+++ src/main.py
@@ -8,17 +8,29 @@ INPUT_JSON_RESULTS_FILE = os.getenv("INPUT_JSON-RESULTS-FILE")
 INPUT_ENABLE = os.getenv("INPUT_ENABLE")
 INPUT_LOG_LEVEL = os.getenv("INPUT_LOG-LEVEL")
 INPUT_STD = os.getenv("INPUT_STD")
+INPUT_PLATFORM = os.getenv("INPUT_PLATFORM")
 INPUT_SUPPRESS = os.getenv("INPUT_SUPPRESS")
 INPUT_SOURCES = os.getenv("INPUT_SOURCES")
 INPUT_ADDITIONAL_ARGS = os.getenv("INPUT_ADDITIONAL-ARGS")
+INPUT_ANNOTATION_WARNINGS = os.getenv("INPUT_ANNOTATION-WARNINGS")
+INPUT_ANNOTATION_NOTICES = os.getenv("INPUT_ANNOTATION-NOTICES")
+INPUT_ANNOTATION_FAILURES = os.getenv("INPUT_ANNOTATION-FAILURES")
+INPUT_ANNOTATION_LEVEL_DEFAULT = os.getenv("INPUT_ANNOTATION-LEVEL-DEFAULT")
 
 # Constants
 EXECUTABLE = "cppcheck"
 OUTPUT_FILE = "cppcheck-results.xml"
 
 
-def read_input_additional_args():
-    return
+def get_annotation_level(severity):
+    if severity in INPUT_ANNOTATION_NOTICES.split(","):
+        return "notice"
+    if severity in INPUT_ANNOTATION_WARNINGS.split(","):
+        return "warning"
+    if severity in INPUT_ANNOTATION_FAILURES.split(","):
+        return "failure"
+
+    return INPUT_ANNOTATION_LEVEL_DEFAULT
 
 
 def get_args():
@@ -39,6 +51,9 @@ def get_args():
     if INPUT_STD != "":
         args.append(f'--std={INPUT_STD}')
 
+    if INPUT_PLATFORM != "":
+        args.append(f'--platform={INPUT_PLATFORM}')
+
     if INPUT_ADDITIONAL_ARGS != "":
         additional_args = filter(lambda arg: arg != "", INPUT_ADDITIONAL_ARGS.split(" "))
         args.extend(additional_args)
@@ -54,13 +69,14 @@ def parse_cppcheck_xml(reportFile):
     errors = root.find("errors").findall("error")
     annotations = []
     for error in errors:
+        print(error.attrib)
         location = error.find("location")
         if location is None:
             continue
         annotations.append({
             "title": error.get("msg"),
             "message": error.get("verbose").replace(". ", ".\n"),
-            "annotation_level": "warning",
+            "annotation_level": get_annotation_level(error.get("severity")),
             "file": location.get("file"),
             "line": int(location.get("line")),
             "start_column": int(location.get("column")),
@@ -81,6 +97,10 @@ def main():
 
     annotations = parse_cppcheck_xml(OUTPUT_FILE)
     print(json.dumps(annotations, indent=2), file=open(INPUT_JSON_RESULTS_FILE, "w"))
+    print(json.dumps(annotations, indent=2))
+
+    if any(filter(lambda a: a["annotation_level"] == "error", annotations)):
+        return 1
 
 
 if __name__ == '__main__':
