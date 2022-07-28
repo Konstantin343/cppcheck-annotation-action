832c3664ad647e0dccbbe38394ced95b352b0333
Konstantin Krukhmalev <shader42@gmail.com>
2022-07-29 00:09:29 +0300

    Update tests

--- src/tests.py
+++ src/tests.py
@@ -12,7 +12,7 @@ class CppCheckAnnotationTest(unittest.TestCase):
     "title": "Array 'arr[5]' accessed at index 10, which is out of bounds.",
     "message": "Array 'arr[5]' accessed at index 10, which is out of bounds.",
     "annotation_level": "failure",
-    "file": "/home/test/tests/test.cpp",
+    "file": "tests/test.cpp",
     "line": 14,
     "start_column": 8,
     "end_column": 8
@@ -21,7 +21,7 @@ class CppCheckAnnotationTest(unittest.TestCase):
     "title": "Division by zero.",
     "message": "Division by zero.",
     "annotation_level": "failure",
-    "file": "/home/test/tests/test.cpp",
+    "file": "tests/test.cpp",
     "line": 2,
     "start_column": 10,
     "end_column": 10
@@ -30,7 +30,7 @@ class CppCheckAnnotationTest(unittest.TestCase):
     "title": "Division by zero.",
     "message": "Division by zero.",
     "annotation_level": "failure",
-    "file": "/home/test/tests/test.cpp",
+    "file": "tests/test.cpp",
     "line": 4,
     "start_column": 14,
     "end_column": 14
@@ -39,7 +39,7 @@ class CppCheckAnnotationTest(unittest.TestCase):
     "title": "Division by zero.",
     "message": "Division by zero.",
     "annotation_level": "failure",
-    "file": "/home/test/tests/test.cpp",
+    "file": "tests/test.cpp",
     "line": 6,
     "start_column": 10,
     "end_column": 10
@@ -48,7 +48,7 @@ class CppCheckAnnotationTest(unittest.TestCase):
     "title": "Division by zero.",
     "message": "Division by zero.",
     "annotation_level": "failure",
-    "file": "/home/test/tests/test.cpp",
+    "file": "tests/test.cpp",
     "line": 14,
     "start_column": 23,
     "end_column": 23
@@ -57,7 +57,7 @@ class CppCheckAnnotationTest(unittest.TestCase):
     "title": "Variable 'arr[10]' is assigned a value that is never used.",
     "message": "Variable 'arr[10]' is assigned a value that is never used.",
     "annotation_level": "warning",
-    "file": "/home/test/tests/test.cpp",
+    "file": "tests/test.cpp",
     "line": 14,
     "start_column": 13,
     "end_column": 13
@@ -66,7 +66,7 @@ class CppCheckAnnotationTest(unittest.TestCase):
     "title": "Variable 'p' is assigned a value that is never used.",
     "message": "Variable 'p' is assigned a value that is never used.",
     "annotation_level": "warning",
-    "file": "/home/test/tests/test.cpp",
+    "file": "tests/test.cpp",
     "line": 22,
     "start_column": 6,
     "end_column": 6
@@ -75,7 +75,7 @@ class CppCheckAnnotationTest(unittest.TestCase):
     "title": "The function 'f' is never used.",
     "message": "The function 'f' is never used.",
     "annotation_level": "warning",
-    "file": "/home/test/tests/test.cpp",
+    "file": "tests/test.cpp",
     "line": 12,
     "start_column": 0,
     "end_column": 0
